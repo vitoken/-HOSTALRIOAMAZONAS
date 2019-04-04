10f14ab32fcd172a6fab53f189e489825d3049bf
cetecom <cetecom@LCT50415>
2019-04-03 22:34:02 -0300

    Cambios realizados en clases

--- index.php
+++ index.php
@@ -1,17 +1,30 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+<?php
+include "lib/constantes.php";
+?>
+
+
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <link href="css/estiloprincipal.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        
+        <div id="contenedor">
+            <div id="titulo">Index</div>
+            <div id="menu"><?php include('menu.php');?></div>
+            <div id="contenido"></div>
+            
+            
+            <footer>Antonio Varas #666, Fono de contacto: 000000000</footer>
+        </div>
+        
     </body>
+    
+    
 </html>
---
+++ menu.php
@@ -0,0 +1,5 @@
+<div class="list-group">
+    <div><a href="<?=URLBASE;?>contenido/Home.php"<i class="fa fa-home fa-2x"></i> Home</a></div>
+    <div><a href="<?=URLBASE;?>contenido/AboutChile.php"<i class="fa fa-home fa-2x"></i> About Chile</a></div>
+    <div><a href="<?=URLBASE;?>contenido/Reservation.php"<i class="fa fa-home fa-2x"></i> Reservation</a></div>
+</div>
